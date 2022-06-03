5ad74830d689b4188edd5126143e673ead2f2e37
hik8hik <hik8hik@gmail.com>
2022-06-03 07:47:53 +0100

    Dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import "./App.css";
-
 const App = () => {
-  return (
-    <div>
-      <h1>Business</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Business</h1>
+        </div>
+    );
 };
 
 export default App;
