56f40ec47e1e00b5e052a915ebc046598c4fff29
hik8hik <hik8hik@gmail.com>
2022-06-10 10:18:11 +0100

    App start config and template copy

---
+++ src/store/constant.js
@@ -0,0 +1,8 @@
+// theme constant
+export const gridSpacing = 3;
+export const drawerWidth = 260;
+export const appDrawerWidth = 320;
+
+// local //"http://127.0.0.1:5000"
+// online//"https://dry-stream-10813.herokuapp.com"
+export const API_SERVER = 'https://dry-stream-10813.herokuapp.com';
