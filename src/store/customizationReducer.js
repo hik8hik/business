27c4c350afc8079db9cc8e0a9b8c5a738e158793
hik8hik <hik8hik@gmail.com>
2022-06-10 19:12:00 +0100

    From Berry Template

--- src/store/customizationReducer.js
+++ src/store/customizationReducer.js
@@ -1,5 +1,5 @@
 // project imports
-import config from '../config';
+import config from 'config';
 
 // action - state management
 import * as actionTypes from './actions';
@@ -13,7 +13,7 @@ export const initialState = {
 
 // ==============================|| CUSTOMIZATION REDUCER ||============================== //
 
-const customizationReducer = (state = initialState, action = {}) => {
+const customizationReducer = (state = initialState, action) => {
     let id;
     switch (action.type) {
         case actionTypes.MENU_OPEN:
